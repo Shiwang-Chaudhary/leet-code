e411e85c4631219ce254c70dcefeb8c8c805eecc
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-07-06 09:25:51 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (67.48%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,12 +1,40 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        // Brute force solution:
+        //  int n = nums.size();
+        //  for(int i = 0; i < n; i++){
+        //      if(nums[i] == target){
+        //          return i;
+        //      }
+        //  }
+        //  return -1;
+
         int n = nums.size();
-        for(int i = 0; i < n; i++){
-            if(nums[i] == target){
-                return i;
+        int low = 0;
+        int high = n - 1;
+        int index = -1;
+        while (low <= high) {
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target) return mid;
+            //left half is sorted
+            if(nums[low] <= nums[mid]){
+                //element lies in left sorted half
+                if(nums[low]<= target && target < nums[mid]){
+                    high = mid - 1;
+                }else{//element lies in right unsorted half
+                    low = mid + 1;
+                }
+            }else{//right half is sorted
+
+                //element lies in right sorted half
+                if(nums[mid] < target && target <= nums[high]){
+                    low = mid + 1;
+                }else{//element lies in left unsorted half
+                    high = mid - 1;
+                }
             }
         }
-        return -1;
+        return index;
     }
 };
