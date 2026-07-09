8974e2ebafc1c0bb790858811884c7448cd67b08
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-07-09 10:02:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (49.66%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
     int mySqrt(int x) {
+        //Brute force: TC: O(n) and SC: O(1).
+        // long long ans = 0;
+        // for(int i = 0; i <= x; i++){
+        //     ans = 1LL*i*i;
+        //     if(ans == x) return i;
+        //     if(ans > x) return i-1;
+        // }
+        // return -1;
+
+        //Optimal solution: TC: O(logn) and SCL O(1).
         long long ans = 0;
-        for(int i = 0; i <= x; i++){
-            ans = 1LL*i*i;
-            if(ans == x) return i;
-            if(ans > x) return i-1;
+        int low = 0;
+        int high = x;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(1LL*mid * mid > x){
+                high = mid - 1;
+            }else{
+                //when mid * mid <= x
+                ans = mid;
+                low = mid + 1;
+            }
         }
-        return -1;
+        return ans;
     }
 };
