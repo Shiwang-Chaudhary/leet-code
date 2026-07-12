38dc3976c528102620056739ab3292513e649eab
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-07-12 11:11:36 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (29.44%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -10,6 +10,21 @@ public:
     }
 
     int findKthPositive(vector<int>& nums, int k) {
+//         The formula is:
+
+// missing = arr[i] - (i + 1)
+
+// It works only if:
+
+// The array is sorted in increasing order.
+// The array contains distinct positive integers.
+// The indexing is 0-based.
+
+// These are exactly the constraints of LeetCode 1539.
+
+
+
+
         //Brute force: TC: O(n + k) and SC: O(1)
     //    int n = nums.size();
     //    int current = 1;
@@ -27,14 +42,41 @@ public:
     //    return current-1;
 
         //Brute force's Striver solution: TC: O(n) and SC: O(1)
+        // int n = nums.size();
+        // for(int i = 0; i < n; i++){
+        //     if(nums[i] <= k){
+        //         k++;
+        //     }else{
+        //         return k;
+        //     }
+        // }
+        // return k;
+
         int n = nums.size();
-        for(int i = 0; i < n; i++){
-            if(nums[i] <= k){
-                k++;
+        int low = 0;
+        int high = n-1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            int totalmissing = nums[mid] - (mid+1);
+            if(totalmissing < k){
+                low = mid + 1;
             }else{
-                return k;
+                high = mid - 1;
             }
         }
-        return k;
+        //Ans should be below:
+        // int missingtillHigh = nums[high] - (high + 1);
+        // int more = k - missingtillHigh;
+        
+        //but we did low + k because:
+        /*
+        //optimal solution: why we did ans = low+k at the end instead of ans = arr[high] + more?
+        reason: more = k - missingtillhigh, missingtillhigh = arr[high] - (high+1),
+        so, ans = arr[high] + (k - missingtillhigh) => ans = arr[high] + k - (arr[high] - (high+1))
+        so, ans becomes, ans = arr[high] + k - arr[high] + high + 1;
+        cancel out arr[high]: ans = high + k + 1;
+        since low is always 1 more than high, we can say that: ans = low + k;
+        */
+        return low + k;
     }
 };
