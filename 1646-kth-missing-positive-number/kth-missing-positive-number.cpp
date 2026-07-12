d3159756472797e3b0a4cd7aeda5fe9396cb48c6
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-07-12 09:40:46 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (29.44%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -10,19 +10,31 @@ public:
     }
 
     int findKthPositive(vector<int>& nums, int k) {
-       int n = nums.size();
-       int current = 1;
-       int index = 0;
-       int missing = 0;
-       while(missing < k){
-            if(index < n && current == nums[index]){
-                index++;
+        //Brute force: TC: O(n + k) and SC: O(1)
+    //    int n = nums.size();
+    //    int current = 1;
+    //    int index = 0;
+    //    int missing = 0;
+    //    while(missing < k){
+    //         if(index < n && current == nums[index]){
+    //             index++;
+    //         }else{
+    //             missing++;
+    //             if(missing == k) return current;
+    //         }
+    //         current++;
+    //    }
+    //    return current-1;
+
+        //Brute force's Striver solution: TC: O(n) and SC: O(1)
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            if(nums[i] <= k){
+                k++;
             }else{
-                missing++;
-                if(missing == k) return current;
+                return k;
             }
-            current++;
-       }
-       return current-1;
+        }
+        return k;
     }
 };
