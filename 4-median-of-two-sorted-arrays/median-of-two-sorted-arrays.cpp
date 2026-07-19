31723c8d7b4ef8d6d7d767b9291ffe9b68278b87
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-07-19 13:16:57 +0530

    Time: 0 ms (100.00%) | Memory: 95.2 MB (61.76%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -20,38 +20,85 @@ public:
         // return median;
 
         //Better solution: TC: O(n+m) and SC: O(n+m) due to extra array
+        // int i = 0;
+        // int j = 0;
+        // int n = nums1.size();
+        // int m = nums2.size();
+        // vector<int> sorted;
+        // while(i<n && j<m){
+        //     if(nums1[i] < nums2[j]){
+        //         sorted.push_back(nums1[i]);
+        //         i++;
+        //     }else{
+        //         sorted.push_back(nums2[j]);
+        //         j++;
+        //     }
+        // }
+        // while(i<n){
+        //     sorted.push_back(nums1[i]);
+        //     i++;
+        // }
+        // while(j<m){
+        //     sorted.push_back(nums2[j]);
+        //     j++;
+        // }
+        // int s = n+m;
+        // double median = 0;
+        // if(s%2 == 0){
+        //     //even
+        //     int middle = s/2;
+        //     median = (sorted[middle] + sorted[middle-1])/2.0;
+        // }else{
+        //     //odd
+        //     int middle = s/2;
+        //     median = sorted[middle];
+        // }
+        // return median;
+
+        //Better solution 2: TC: O(n+m) and SC: O(1)
+        //Same as better solution but we didnt take extra array but instead take current and previous element since we only need those at mid index of merged sorted array.
         int i = 0;
         int j = 0;
         int n = nums1.size();
         int m = nums2.size();
-        vector<int> sorted;
+        int current = 0;
+        int prev = 0;
+        int count = -1;
+        int s = m+n;
+        int mid = s/2;
         while(i<n && j<m){
+            prev = current;
             if(nums1[i] < nums2[j]){
-                sorted.push_back(nums1[i]);
+                current = nums1[i];
                 i++;
             }else{
-                sorted.push_back(nums2[j]);
+                current = nums2[j];
                 j++;
             }
+            count ++;
+            if(count == mid){
+                break;
+            }
         }
-        while(i<n){
-            sorted.push_back(nums1[i]);
-            i++;
+        while (count < mid && i < n) {
+            prev = current;
+            current = nums1[i++];
+            count++;
         }
-        while(j<m){
-            sorted.push_back(nums2[j]);
-            j++;
+
+        // Process remaining elements of nums2
+        while (count < mid && j < m) {
+            prev = current;
+            current = nums2[j++];
+            count++;
         }
-        int s = n+m;
         double median = 0;
         if(s%2 == 0){
             //even
-            int middle = s/2;
-            median = (sorted[middle] + sorted[middle-1])/2.0;
+            median = (current + prev)/2.0;
         }else{
             //odd
-            int middle = s/2;
-            median = sorted[middle];
+            median = current;
         }
         return median;
     }
