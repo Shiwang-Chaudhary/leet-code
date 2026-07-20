de3e96a80e72f2d7d062ab4e304298e9196b67e0
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-07-20 13:19:12 +0530

    Add Medium C++ Solution from GeeksforGeeks

--- GeeksforGeeks/C++/Medium/K-th element of two Arrays/Solution.cpp
+++ GeeksforGeeks/C++/Medium/K-th element of two Arrays/Solution.cpp
@@ -1,28 +1,64 @@
 class Solution {
   public:
     int kthElement(vector<int> &a, vector<int> &b, int k) {
-        // code here
-        vector<int> sorted;
+        
+        //Better solution 1: TC: O(m+n) and SC: O(m+n)
+        // vector<int> sorted;
+        // int n = a.size();
+        // int m = b.size();
+        // int i = 0, j = 0;
+        // while(i < n && j < m){
+        //     if(a[i] < b[j]){
+        //         sorted.push_back(a[i]);
+        //         i++;
+        //     }else{
+        //         sorted.push_back(b[j]);
+        //         j++;
+        //     }
+        // }
+        // while(i < n){
+        //     sorted.push_back(a[i]);
+        //     i++;
+        // }
+        // while(j < m){
+        //     sorted.push_back(b[j]);
+        //     j++;
+        // }
+        // return sorted[k-1];
+        
+        //Better solution 2: TC: O(n+m) and SC: O(1)
         int n = a.size();
         int m = b.size();
-        int i = 0, j = 0;
+        int current = 0;
+        int count = 0;
+        int i = 0;
+        int j = 0;
+        
         while(i < n && j < m){
             if(a[i] < b[j]){
-                sorted.push_back(a[i]);
+                current = a[i];
                 i++;
             }else{
-                sorted.push_back(b[j]);
+                current = b[j];
                 j++;
+            }
+            count++;
+            if(count == k){
+                return current;
             }
         }
         while(i < n){
-            sorted.push_back(a[i]);
+            current = a[i];
             i++;
+            count++;
+            if(count == k) return current;
         }
         while(j < m){
-            sorted.push_back(b[j]);
+            current = b[j];
             j++;
+            count++;
+            if(count == k) return current;
         }
-        return sorted[k-1];
+        return 0;
     }
 };
