af30e85ce60a30e166631bf5f4d38fd63343cc0d
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-07-23 10:28:52 +0530

    Add C++ Solution to Patterns/Binary Search

--- LeetCode/Patterns/Binary Search/240. Search a 2D Matrix II/Solution.cpp
+++ LeetCode/Patterns/Binary Search/240. Search a 2D Matrix II/Solution.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        //Brute force:
+        // //Brute force: TC: O(n^2) and SC: O(1)
+        // int row = matrix.size();
+        // int col = matrix[0].size();
+        // for(int i = 0; i < row; i++){
+            
+        //     for(int j = 0; j < col; j++){
+        //         if(matrix[i][j] == target){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
         int row = matrix.size();
         int col = matrix[0].size();
         for(int i = 0; i < row; i++){
-            
-            for(int j = 0; j < col; j++){
-                if(matrix[i][j] == target){
+            int low = 0, high = col-1;
+            while(low <= high){
+                int mid = low + (high - low)/2;
+                if(matrix[i][mid] == target){
                     return true;
+                }else if(matrix[i][mid] < target){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
                 }
             }
         }
