9afd27b899af6894141d0a0cdb7b67999f222b37
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-07-30 12:56:21 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/14. Longest Common Prefix/Solution.cpp
+++ LeetCode/C++/Easy/14. Longest Common Prefix/Solution.cpp
@@ -2,10 +2,13 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         string first = strs[0];
+        //compare each element of first with every element of strs array
         for(int i = 0; i < first.size(); i++){
             for(int j = 0; j < strs.size(); j++){
+                //compare ith element of first with ith element of strs[j]
                 string el = strs[j];
                 if(i > el.size()-1 || first[i] != el[i]){
+                    //if first element isnt common then return empty string
                     if(i == 0){
                         return "";
                     }
@@ -14,6 +17,7 @@ public:
             }
             
         }
+        //We are returning first because there might be a case where all the elements in strs array have same value like strs = [abc,abc,abc] so in that case whole first is the answer
         return first;
     }
 };
