e452ca34e3db9d5ab653fc0ef2d9a2cc30e1bfa7
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-07-30 15:00:34 +0530

    Add C++ Solution to Patterns/Strings

--- LeetCode/Patterns/Strings/812. Rotate String/Solution.cpp
+++ LeetCode/Patterns/Strings/812. Rotate String/Solution.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-
-    // void rev(string &s){
-    //     int n = s.size();
-    //     int left = 0;
-    //     int right = n-1;
-    //     while(low < high){
-
-    //     }
-    // }
-
     bool rotateString(string s, string goal) {
-        int n = s.size();
-        string original = s;
-        if(s == goal) return true;
-        for(int i = 1; i < n; i++){
-            //we are doing s = original because in each iteration we need original string instead of the one that we modified before.
-            s = original;
-            reverse(s.begin(), s.begin()+i);
-            reverse(s.begin()+i, s.end());
-            reverse(s.begin(), s.end());
-            if(s == goal){
-                return true;
+        //Brute force: TC: O(n^2) and SC: O(n) because we are storing s inside original variable
+        // int n = s.size();
+        // string original = s;
+        // if(s == goal) return true;
+        // for(int i = 1; i < n; i++){
+        //     //we are doing s = original because in each iteration we need original string instead of the one that we modified before.
+        //     s = original;
+        //     reverse(s.begin(), s.begin()+i);
+        //     reverse(s.begin()+i, s.end());
+        //     reverse(s.begin(), s.end());
+        //     if(s == goal){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+
+        //Optimal solution: TC: O(n) and SC: O(n)
+        string newString = s+s;
+        int size = s.size();
+        for(int i = 0; i < newString.size(); i++){
+            if(newString[i] == goal[0]){
+                //could be the goal
+                string sub = newString.substr(i, size);
+                if(sub == goal) return true;
             }
         }
         return false;
