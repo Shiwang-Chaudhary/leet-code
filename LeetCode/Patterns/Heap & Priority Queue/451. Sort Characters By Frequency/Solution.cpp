17e94024cec8872c73bfb18430a0bf2552c5209e
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-08-01 14:28:59 +0530

    Add C++ Solution to Patterns/Heap & Priority Queue

--- LeetCode/Patterns/Heap & Priority Queue/451. Sort Characters By Frequency/Solution.cpp
+++ LeetCode/Patterns/Heap & Priority Queue/451. Sort Characters By Frequency/Solution.cpp
@@ -1,24 +1,62 @@
 class Solution {
 public:
     string frequencySort(string s) {
+        //Brute force: O(n^2) and SC:O(n)
+        // string ans;
+        // int n = s.size();
+        // vector<pair<int,char>> v;
+        // sort(s.begin(), s.end());
+        // for(int i = 0; i < n; i++){
+        //     int count = 0;
+        //     for(int j = 0; j < n; j++){
+        //         if(s[i] == s[j]) count++;
+
+        //     }
+        //     if(v.empty() || v.back().second != s[i]){
+        //         v.push_back({count, s[i]});
+        //     }
+        // }
+        // sort(v.begin(), v.end(), greater<pair<int, char>>());
+        // for(auto it: v){
+        //     ans.append(it.first, it.second);
+        // }
+        // return ans;
 
         //Better solution: 
-        int n = s.size();
-        priority_queue<pair<int,char>> pq;
-        unordered_map<char,int> freq;
+        // int n = s.size();
+        // priority_queue<pair<int,char>> pq;
+        // unordered_map<char,int> freq;
+        // string ans;
+        // for(int i = 0; i < n; i++){
+        //     freq[s[i]]++;
+        // }
+        // for(auto it: freq){
+        //     pq.push({it.second, it.first});
+        // }
+        // while(!pq.empty()){
+        //     auto top = pq.top();
+        //     pq.pop();
+        //     int freq = top.first;
+        //     char el = top.second;
+        //     ans.append(freq, el);
+        // }
+        // return ans;
+
+        //Optimal solution :
+        unordered_map<char,int> mp;
         string ans;
-        for(int i = 0; i < n; i++){
-            freq[s[i]]++;
+        int n = s.size();
+        vector<vector<char>> bucket(n+1);
+        for(int i = 0; i < s.size(); i++){
+            mp[s[i]]++;
         }
-        for(auto it: freq){
-            pq.push({it.second, it.first});
+        for(auto it: mp){
+            bucket[it.second].push_back(it.first);
         }
-        while(!pq.empty()){
-            auto top = pq.top();
-            pq.pop();
-            int freq = top.first;
-            char el = top.second;
-            ans.append(freq, el);
+        for(int i = n; i >= 1; i--){
+            for(char c: bucket[i]){
+                ans.append(i,c);
+            }
         }
         return ans;
     }
