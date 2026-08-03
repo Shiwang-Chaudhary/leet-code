87aa37a4a30d61067c5b92e5466f13bc532bafb2
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-08-03 21:07:35 +0530

    Add C++ Solution to Patterns/Strings

--- LeetCode/Patterns/Strings/8. String to Integer atoi/Solution.cpp
+++ LeetCode/Patterns/Strings/8. String to Integer atoi/Solution.cpp
@@ -9,56 +9,40 @@ public:
         long long ans = 0;
         string temp;
         int i = 0;
-
+        bool isNegative = false;
         // Skip leading spaces
-        while (i < n && s[i] == ' ')
-            i++;
-
-        // Handle sign
-        if (i < n && s[i] == '+') {
-            temp = ' ';
-            i++;
-        } else if (i < n && s[i] == '-') {
-            temp = '-';
+        while (i < n && s[i] == ' ') i++;
+        if (i < n && s[i] == '+') i++;
+        else if (i < n && s[i] == '-') {
+            isNegative = true;
             i++;
         }
+        while (i < n && s[i] == '0') i++;
+        if (i == n) return 0;
+        if (!isDigit(s[i])) return 0;
 
-        // Skip leading zeros
-        while (i < n && s[i] == '0')
-            i++;
-
-        // No digits
-        if (i == n)
-            return 0;
-
-        if (!isDigit(s[i]))
-            return 0;
-
-        // Store digits
         while (i < n && isDigit(s[i])) {
             temp += s[i];
             i++;
         }
 
-        // Manual conversion with overflow check
+        //Since we cant use stoi
         for (char ch : temp) {
-            if (ch == ' ' || ch == '-')
-                continue;
-
+            if (ch == ' ' || ch == '-') continue;
             int digit = ch - '0';
-
+            //Overflow check:
             if (ans > INT_MAX / 10 ||
                 (ans == INT_MAX / 10 &&
-                 digit > ((!temp.empty() && temp[0] == '-') ? 8 : 7))) {
-                return (!temp.empty() && temp[0] == '-') ? INT_MIN : INT_MAX;
+                 digit > ((!temp.empty() && isNegative) ? 8 : 7))) {
+                return (!temp.empty() && isNegative) ? INT_MIN : INT_MAX;
             }
 
             ans = ans * 10 + digit;
         }
 
-        if (!temp.empty() && temp[0] == '-')
+        if (!temp.empty() && isNegative)
             ans = -ans;
 
-        return (int)ans;
+        return ans;
     }
 };
