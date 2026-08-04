c198b2e5c90806bd01bb727c1ad67649864253b6
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-08-04 15:50:14 +0530

    Add Easy C++ Solution from LeetCode

---
+++ LeetCode/C++/Easy/125. Valid Palindrome/Solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.size();
+        int left = 0;
+        int right = n-1;
+        while(left<right){
+            if(!isalnum(s[left])){
+                left++;
+                continue;
+            }else if(!isalnum(s[right])){
+                right--;
+                continue;
+            }else if(tolower(s[left])!=tolower(s[right])) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
