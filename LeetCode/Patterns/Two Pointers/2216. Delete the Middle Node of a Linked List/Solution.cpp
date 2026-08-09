2bb16bb77dadf7067f57c60a70407c79aed5c982
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-08-09 16:42:28 +0530

    Add C++ Solution to Patterns/Two Pointers

--- LeetCode/Patterns/Two Pointers/2216. Delete the Middle Node of a Linked List/Solution.cpp
+++ LeetCode/Patterns/Two Pointers/2216. Delete the Middle Node of a Linked List/Solution.cpp
@@ -11,25 +11,39 @@
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        if (head == nullptr) return nullptr;
-        int size = 0;
-        ListNode* temp = head;
-        while(temp != nullptr){
-            size++;
-            temp = temp->next;
+        //Brute force: TC: O(2n) and SC: O(1)
+        // if (head == nullptr) return nullptr;
+        // int size = 0;
+        // ListNode* temp = head;
+        // while(temp != nullptr){
+        //     size++;
+        //     temp = temp->next;
+        // }
+        // if(head->next == nullptr) return nullptr;
+        // temp = head;
+        // int count = 0;
+        // int mid = size/2 + 1;
+        // while(temp != nullptr){
+        //     count++;
+        //     if(count == mid - 1){
+        //         temp->next = temp->next->next;
+        //         break;
+        //     }
+        //     temp = temp->next;
+        // }
+        // return head;
+
+        //Optimal solution: TC: O(n) and SC: O(1)
+        if(head == nullptr || head->next == nullptr) return nullptr;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* back = slow;
+        while(fast != nullptr && fast->next != nullptr){
+            back = slow;
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        if(head->next == nullptr) return nullptr;
-        temp = head;
-        int count = 0;
-        int mid = size/2 + 1;
-        while(temp != nullptr){
-            count++;
-            if(count == mid - 1){
-                temp->next = temp->next->next;
-                break;
-            }
-            temp = temp->next;
-        }
+        back->next = back->next->next;
         return head;
     }
 };
