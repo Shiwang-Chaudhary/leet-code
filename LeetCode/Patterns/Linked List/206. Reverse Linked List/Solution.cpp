9effc68a3c70a1004fd7922af93e34c0b87aae02
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-08-10 05:50:50 +0530

    Add C++ Solution to Patterns/Linked List

--- LeetCode/Patterns/Linked List/206. Reverse Linked List/Solution.cpp
+++ LeetCode/Patterns/Linked List/206. Reverse Linked List/Solution.cpp
@@ -12,28 +12,33 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         //Brute force: TC:O(n) and SC:O(1)
-        stack<int> st;
-        ListNode* temp = head;
-        while(temp != nullptr){
-            st.push(temp->val);
-            temp = temp->next;
-        }
-        temp = head;
-        while(temp != nullptr){
-            int el = st.top();
-            st.pop();
-            temp->val = el;
-            temp = temp->next;
-        }
-        return head;
-
-
-
-        // ListNode* curr = NULL;
-        // ListNode* front = NULL;
-        // ListNode* extra = NULL;
-        // while(curr != nullptr){
-
+        // stack<int> st;
+        // ListNode* temp = head;
+        // while(temp != nullptr){
+        //     st.push(temp->val);
+        //     temp = temp->next;
         // }
+        // temp = head;
+        // while(temp != nullptr){
+        //     int el = st.top();
+        //     st.pop();
+        //     temp->val = el;
+        //     temp = temp->next;
+        // }
+        // return head;
+
+
+
+        ListNode* curr = head;
+        ListNode* front = NULL;
+        ListNode* prev = NULL;
+        while(curr != nullptr){
+            front = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = front;
+        }
+        head = prev;
+        return head;
     }
 };
