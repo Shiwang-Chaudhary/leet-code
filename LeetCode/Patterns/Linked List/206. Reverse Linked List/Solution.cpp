3acea14c78316649fc26d8d56715b92d8470904a
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-08-10 06:30:31 +0530

    Add C++ Solution to Patterns/Linked List

--- LeetCode/Patterns/Linked List/206. Reverse Linked List/Solution.cpp
+++ LeetCode/Patterns/Linked List/206. Reverse Linked List/Solution.cpp
@@ -10,8 +10,9 @@
  */
 class Solution {
 public:
+
     ListNode* reverseList(ListNode* head) {
-        //Brute force: TC:O(n) and SC:O(1)
+        //Brute force: TC:O(2n) and SC:O(n) because of stack
         // stack<int> st;
         // ListNode* temp = head;
         // while(temp != nullptr){
@@ -28,17 +29,27 @@ public:
         // return head;
 
 
+        //Optimal solution: TC: O(n) and SC:O(1)
+        //Also called Three pointer approach or Iterative approach
+        // ListNode* curr = head;
+        // ListNode* front = NULL;
+        // ListNode* prev = NULL;
+        // while(curr != nullptr){
+        //     front = curr->next;
+        //     curr->next = prev;
+        //     prev = curr;
+        //     curr = front;
+        // }
+        // head = prev;
+        // return head;
 
-        ListNode* curr = head;
-        ListNode* front = NULL;
-        ListNode* prev = NULL;
-        while(curr != nullptr){
-            front = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = front;
-        }
-        head = prev;
-        return head;
+        //Good for learning:
+        //Recursion approach: TC:O(n) and SC:O(n) because of recursion stack
+        if(head == nullptr || head->next == nullptr) return head;
+        ListNode* newHead = reverseList(head->next);
+        ListNode* front = head->next;
+        front->next = head;
+        head->next = nullptr;
+        return newHead;
     }
 };
