850f41699de63996ffc740ebca9b4a2fa0331444
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-08-13 13:50:19 +0530

    Add Medium C++ Solution from LeetCode

--- LeetCode/C++/Medium/19. Remove Nth Node From End of List/Solution.cpp
+++ LeetCode/C++/Medium/19. Remove Nth Node From End of List/Solution.cpp
@@ -25,25 +25,68 @@ public:
     }
 
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        //Brute force: TC: O(3n) and SC: O(1)
+        // if(head == nullptr || head->next == nullptr) return nullptr;
+        // ListNode* temp = head;
+        // ListNode* revHead = reverseLL(temp);
+        // if(n == 1){
+        //     revHead = revHead->next;
+        //     head = reverseLL(revHead);
+        //     return head;
+        // }
+        // int count = 0;
+        // ListNode* current = revHead;
+        // while(current != nullptr){
+        //     count++;
+        //     if(count == n-1){
+        //         current->next = current->next->next;
+        //         break;
+        //     }
+        //     current = current->next;
+        // }
+        // head = reverseLL(revHead);
+        // return head;
+
+        //Brute solution 2: TC: O(n) and SC: O(1)
+        // if(head == nullptr || head->next == nullptr) return nullptr;
+        // int size = 0;
+        // ListNode* temp = head;
+        // while(temp != nullptr){
+        //     size++;
+        //     temp = temp->next;
+        // }
+        // int newPos = size - n + 1;
+        // temp = head;
+        // if(newPos == 1){
+        //     head = head->next;
+        //     return head;
+        // }
+        // int count = 0;
+        // while(temp != nullptr){
+        //     count++;
+        //     if(count == newPos-1){
+        //         temp->next = temp->next->next;
+        //         break;
+        //     }
+        //     temp = temp->next;
+        // }
+        // return head;
+
+        //Optimal solution: TC: O(n) and SC: O(1)
         if(head == nullptr || head->next == nullptr) return nullptr;
-        ListNode* temp = head;
-        ListNode* revHead = reverseLL(temp);
-        if(n == 1){
-            revHead = revHead->next;
-            head = reverseLL(revHead);
-            return head;
+        ListNode* fast = head;
+        for(int i = 0; i < n; i++){
+            fast = fast->next;
         }
-        int count = 0;
-        ListNode* current = revHead;
-        while(current != nullptr){
-            count++;
-            if(count == n-1){
-                current->next = current->next->next;
-                break;
-            }
-            current = current->next;
+        //if n = size of LL then fast becomes null and that happens only when we have to delete the head node, so:
+        if(fast == nullptr) return head = head->next;
+        ListNode* slow = head;
+        //since fast == nullptr condition is already handled we dont need to use fast!=nullptr in while loop and since we are moving fast for 1 element at a time, fast will never be nullptr before the end of loop...
+        while(fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next;
         }
-        head = reverseLL(revHead);
+        slow->next = slow->next->next;
         return head;
     }
 };
