cfbd8d3046f0c2769d25baec3f2657a9a0661a9e
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-08-17 11:07:34 +0530

    Add C++ Solution to Patterns/Linked List

---
+++ GeeksforGeeks/Patterns/Linked List/Sort a linked list of 0s 1s and 2s/Solution.cpp
@@ -0,0 +1,45 @@
+/* Node is defined as
+  class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* segregate(Node* head) {
+        // code here
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        Node* temp = head;
+        while(temp != nullptr){
+            if(temp->data == 0) count0++;
+            else if(temp->data == 1) count1++;
+            else count2++;
+            temp = temp->next;
+        }
+        temp = head;
+        while(count0 != 0){
+            count0--;
+            temp->data = 0;
+            temp = temp->next;
+        }
+        while(count1 != 0){
+            count1--;
+            temp->data = 1;
+            temp = temp->next;
+        }
+        while(count2 != 0){
+            count2--;
+            temp->data = 2;
+            temp = temp->next;
+        }
+        return head;
+    }
+};
