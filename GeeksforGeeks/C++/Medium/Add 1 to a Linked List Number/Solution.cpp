677ff73a191f0bc4f1813c7b46d5b1ec35268f2f
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-08-18 06:32:24 +0530

    Add Medium C++ Solution from GeeksforGeeks

--- GeeksforGeeks/C++/Medium/Add 1 to a Linked List Number/Solution.cpp
+++ GeeksforGeeks/C++/Medium/Add 1 to a Linked List Number/Solution.cpp
@@ -25,26 +25,47 @@ class Solution {
         }
         return prev;
     }
+    
+    int func(Node* head){
+        if(head == nullptr) return 1;
+        int carry = func(head->next);
+        int sum = carry + head->data;
+        int digit = sum % 10;
+        head->data = digit;
+        carry = sum / 10;
+        return carry;
+    }
   
     Node* addOne(Node* head) {
-        int carry = 1;
+        //Optimal solution: TC: O(n) and SC: O(1)
+        // int carry = 1;
+        // Node* temp = head;
+        // Node* revHead = reverseLL(temp);
+        // temp = revHead;
+        // Node* prev = temp;
+        // while(temp != nullptr){
+        //     int sum = temp->data + carry;
+        //     int digit = sum % 10;
+        //     carry = sum / 10;
+        //     temp->data = digit;
+        //     prev = temp;
+        //     temp = temp->next;
+        // }
+        // if(carry != 0){
+        //     Node* lastNode = new Node(carry);
+        //     prev->next = lastNode;
+        // }
+        // head = reverseLL(revHead);
+        // return head;
+        
+        //Not optimal: O(n) and SC: O(1)
         Node* temp = head;
-        Node* revHead = reverseLL(temp);
-        temp = revHead;
-        Node* prev = temp;
-        while(temp != nullptr){
-            int sum = temp->data + carry;
-            int digit = sum % 10;
-            carry = sum / 10;
-            temp->data = digit;
-            prev = temp;
-            temp = temp->next;
+        int carry = func(temp);
+        if (carry) {
+            Node* newNode = new Node(carry);
+            newNode->next = head;
+            head = newNode;
         }
-        if(carry != 0){
-            Node* lastNode = new Node(carry);
-            prev->next = lastNode;
-        }
-        head = reverseLL(revHead);
         return head;
     }
 };
