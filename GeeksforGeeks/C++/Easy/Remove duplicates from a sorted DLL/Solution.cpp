a98c254604a0c3b5c2a56407b698b49471210d8e
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-08-19 14:46:57 +0530

    Add Easy C++ Solution from GeeksforGeeks

--- GeeksforGeeks/C++/Easy/Remove duplicates from a sorted DLL/Solution.cpp
+++ GeeksforGeeks/C++/Easy/Remove duplicates from a sorted DLL/Solution.cpp
@@ -14,18 +14,33 @@ class Node {
 class Solution {
   public:
     Node* removeDuplicates(Node* head) {
-        // code here
-        unordered_set<int> st;
+        //Better solution: TC: O(n) and SC: O(n)
+        // unordered_set<int> st;
+        // Node* temp = head;
+        // while(temp != nullptr){
+        //     Node* front = temp->next;
+        //     Node* back = temp->prev;
+        //     if(st.find(temp->data) != st.end()){
+        //         if(back != nullptr) back->next = front;
+        //         if(front != nullptr) front->prev = back;
+        //         temp = front;
+        //     }else{
+        //         st.insert(temp->data);
+        //         temp = temp->next;
+        //     }
+        // }
+        // return head;
+        
+        //Optimal solution: TC: O(n) and SC: O(1)
         Node* temp = head;
-        while(temp != nullptr){
+        while(temp != nullptr && temp->next != nullptr){
             Node* front = temp->next;
             Node* back = temp->prev;
-            if(st.find(temp->data) != st.end()){
-                if(back != nullptr) back->next = front;
-                if(front != nullptr) front->prev = back;
-                temp = front;
+            if(temp->data == front->data){
+                temp->next = front->next;
+                if(temp->next != nullptr) temp->next->prev = temp;
+                //No need to remove temp ahead because the next duplicate is deleted now
             }else{
-                st.insert(temp->data);
                 temp = temp->next;
             }
         }
