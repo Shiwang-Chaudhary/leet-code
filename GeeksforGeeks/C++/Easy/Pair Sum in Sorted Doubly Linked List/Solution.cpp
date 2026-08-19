5822d6c8171012476d4971d8fa565a094a46d845
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-08-19 12:23:15 +0530

    Add Easy C++ Solution from GeeksforGeeks

--- GeeksforGeeks/C++/Easy/Pair Sum in Sorted Doubly Linked List/Solution.cpp
+++ GeeksforGeeks/C++/Easy/Pair Sum in Sorted Doubly Linked List/Solution.cpp
@@ -15,6 +15,33 @@ class Node {
 class Solution {
   public:
     vector<vector<int>> givenSumPairs(Node* head, int target) {
+        //Brute force: TC: O(n^2) and SC: O(1)
+        // vector<vector<int>> ans;
+        // for (Node* first = head; first != nullptr; first = first->next) {
+        //     for (Node* second = first->next; second != nullptr; second = second->next) {
+        //         if (first->data + second->data == target) {
+        //             ans.push_back({first->data, second->data});
+        //         }
+        //     }
+        // }
+        // return ans;
+        
+        //Better solution: TC: O(n) and SC: O(n)
+        //Hashset solution:
+        // vector<vector<int>> ans;
+        // unordered_set<int> st;
+        // Node* temp = head;
+        // while (temp != nullptr) {
+        //     int remaining = target - temp->data;
+        //     if (st.find(remaining) != st.end()) {
+        //         ans.push_back({remaining, temp->data});
+        //     }
+        //     st.insert(temp->data);
+        //     temp = temp->next;
+        // }
+        // return ans;
+        
+        //Optimal solution: TC: O(n) and SC: O(1)
         //Since list is sorted we can use two pointers approach:
         vector<vector<int>> ans;
         Node* temp = head;
