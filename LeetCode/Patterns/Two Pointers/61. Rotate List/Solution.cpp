0bb5fbc055776697bd435a4234418a1affb22338
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-08-20 15:47:32 +0530

    Add C++ Solution to Patterns/Two Pointers

---
+++ LeetCode/Patterns/Two Pointers/61. Rotate List/Solution.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head == nullptr) return head;
+        ListNode* last = head;
+        int size = 0;
+        ListNode* reqNode = head;
+        while(last->next != nullptr){
+            size++;
+            last = last->next;
+        }
+        //Total size of LL and last points to the last node of LL
+        size = size + 1; // we did +1 because we didnt include lastnode in size
+        //we need to be on this position...
+        int rot = k % size;
+        //if rotations are same as size then no need to do any rotation and just return head
+        if(rot == 0) return head;
+        int steps = size - rot;
+        int count = 0;
+        //Now it becomes a circular LL
+        last->next = head;
+        while(count <= steps){
+            count++;
+            if(count == steps){
+                //New head
+                head = reqNode->next;
+                reqNode->next = nullptr;
+                break;
+            }else{
+                reqNode = reqNode->next;
+            }
+        }
+        return head;
+    }
+};
