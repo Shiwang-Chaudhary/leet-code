704b9a638ecb92b160fe246be2b29a6612bcbeb8
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-08-21 15:53:38 +0530

    Add C++ Solution to Patterns/Linked List

---
+++ LeetCode/Patterns/Linked List/138. Copy List with Random Pointer/Solution.cpp
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        Node* temp = head;
+        Node* dummy = new Node(-1);
+        Node* mover = dummy;
+                 //Original:Copy
+        unordered_map<Node*, Node*> mp;
+        while(temp != nullptr){
+            Node* newNode = new Node(temp->val);
+            mp[temp] = newNode;
+            mover->next = newNode;
+            temp = temp->next;
+            mover = mover->next;
+        }
+        //Now we have stored Orignal add to Copied address, now we can find the random node for copied list
+        temp = head;
+        Node* newTemp = dummy->next;
+        while(temp != nullptr){
+            Node* randomNode = mp[temp->random];
+            newTemp->random = randomNode;
+            newTemp = newTemp->next;
+            temp = temp->next;
+        }
+        return dummy->next;
+    }
+};
