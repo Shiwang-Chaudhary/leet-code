db283a08bf5cd751603755d78346e2e7bee2a2f5
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-08-22 11:06:59 +0530

    Add C++ Solution to Patterns/Linked List

--- LeetCode/Patterns/Linked List/138. Copy List with Random Pointer/Solution.cpp
+++ LeetCode/Patterns/Linked List/138. Copy List with Random Pointer/Solution.cpp
@@ -18,25 +18,52 @@ class Solution {
 public:
     Node* copyRandomList(Node* head) {
         //Brute solution: TC: O(2n) and SC:O(2n) (hashmap and LL) techincally only O(n) because new LL is just for answer
+        // Node* temp = head;
+        // Node* dummy = new Node(-1);
+        // Node* mover = dummy;
+        //          //Original:Copy
+        // unordered_map<Node*, Node*> mp;
+        // while(temp != nullptr){
+        //     Node* newNode = new Node(temp->val);
+        //     mp[temp] = newNode;
+        //     mover->next = newNode;
+        //     temp = temp->next;
+        //     mover = mover->next;
+        // }
+        // //Now we have stored Orignal add to Copied address, now we can find the random node for copied list
+        // temp = head;
+        // Node* newTemp = dummy->next;
+        // while(temp != nullptr){
+        //     Node* copyRandomNode = mp[temp->random];
+        //     newTemp->random = copyRandomNode;
+        //     newTemp = newTemp->next;
+        //     temp = temp->next;
+        // }
+        // return dummy->next;
+
+        //Optimal solution: O(n) and SC: O(1) but O(n) if we include the answer LL
         Node* temp = head;
         Node* dummy = new Node(-1);
         Node* mover = dummy;
-                 //Original:Copy
-        unordered_map<Node*, Node*> mp;
         while(temp != nullptr){
+            Node* front = temp->next;
             Node* newNode = new Node(temp->val);
-            mp[temp] = newNode;
-            mover->next = newNode;
-            temp = temp->next;
-            mover = mover->next;
+            temp->next = newNode;
+            newNode->next = front;
+            temp = front;
         }
-        //Now we have stored Orignal add to Copied address, now we can find the random node for copied list
         temp = head;
-        Node* newTemp = dummy->next;
         while(temp != nullptr){
-            Node* copyRandomNode = mp[temp->random];
-            newTemp->random = copyRandomNode;
-            newTemp = newTemp->next;
+            Node* copyNode = temp->next;
+            Node* randomCopynode = (temp->random != nullptr) ? temp->random->next : nullptr;
+            copyNode->random = randomCopynode;
+            temp = temp->next->next;
+        }
+        temp = head;
+        while(temp != nullptr){
+            mover->next = temp->next;
+            mover = mover->next;
+            temp->next = temp->next->next;
             temp = temp->next;
         }
         return dummy->next;
