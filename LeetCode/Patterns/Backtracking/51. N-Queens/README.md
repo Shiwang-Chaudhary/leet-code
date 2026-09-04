769436d47250c3e32a800d5d31d93d8e92224238
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-09-04 09:46:15 +0530

    Add Hard C++ Solution for 51. N-Queens from LeetCode

--- LeetCode/Patterns/Backtracking/51. N-Queens/README.md
+++ LeetCode/Patterns/Backtracking/51. N-Queens/README.md
@@ -8,8 +8,8 @@
 Array, Backtracking, Algorithm X
 
 ### 🚀 Performance
-- **Runtime:** 3 ms
-- **Memory:** 12.9 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Patterns/Backtracking/51. N-Queens/Solution.cpp
+++ LeetCode/Patterns/Backtracking/51. N-Queens/Solution.cpp
@@ -48,7 +48,7 @@ public:
     }
 
     vector<vector<string>> solveNQueens(int n) {
-        //Optimal solution: TC: O(n² × n!) and SC: O(n²)
+        //Less optimal solurion: TC: O(N*N!) and SC: O(N^2)... but good for interview
         vector<vector<char>> board(n, vector<char>(n,'.'));
         vector<string> temp;
         vector<vector<string>> ans;
