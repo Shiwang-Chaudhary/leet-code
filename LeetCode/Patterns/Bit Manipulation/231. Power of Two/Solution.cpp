a7b93e31763fa5304d0c938a3dba7195d703b2d5
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-09-09 13:20:10 +0530

    Add Easy C++ Solution for 231. Power of Two from LeetCode

--- LeetCode/Patterns/Bit Manipulation/231. Power of Two/Solution.cpp
+++ LeetCode/Patterns/Bit Manipulation/231. Power of Two/Solution.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+        //For example: n = 16, binary = 10000 and n-1 = 15 => binary = 01111 so 16 & 15 = 0, if ans = 0 then it is power of 2, because every power of 2 has only 1 bit as 1 and every other bit as 0
         if(n <= 0) return false;
         if((n & n-1) == 0) return true;
         return false;
