fe39e115d064a04dade44645ddf962c362e2ed12
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-09-12 11:01:20 +0530

    Add Medium C++ Solution for 29. Divide Two Integers from LeetCode

--- LeetCode/C++/Medium/29. Divide Two Integers/Solution.cpp
+++ LeetCode/C++/Medium/29. Divide Two Integers/Solution.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     int divide(int dividend, int divisor) {
         // Brute force: TC: O(|dividend|), SC: O(1)        
-        long long i = 0;
-        long long sum = 0;
-        bool negative = (dividend < 0) ^ (divisor < 0);
-        long long dividendDummy = dividend;
-        long long divisorDummy = divisor;
+    //     long long i = 0;
+    //     long long sum = 0;
+    //     bool negative = (dividend < 0) ^ (divisor < 0);
+    //     long long dividendDummy = dividend;
+    //     long long divisorDummy = divisor;
 
-        if(dividendDummy < 0){
-            dividendDummy = -dividendDummy;
-        }
-        if(divisorDummy < 0){
-            divisorDummy = -divisorDummy;
-        }
-        while(sum + divisorDummy <= dividendDummy){
-            i++;
-            sum = sum + divisorDummy;
-        }
-       return negative ? -i : i;
+    //     if(dividendDummy < 0){
+    //         dividendDummy = -dividendDummy;
+    //     }
+    //     if(divisorDummy < 0){
+    //         divisorDummy = -divisorDummy;
+    //     }
+    //     while(sum + divisorDummy <= dividendDummy){
+    //         i++;
+    //         sum = sum + divisorDummy;
+    //     }
+    //    return negative ? -i : i;
     }
 };
