c7964e059bc449363487d82b5e4edb05382ff87b
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-09-12 12:20:53 +0530

    Add Medium C++ Solution for 29. Divide Two Integers from LeetCode

--- LeetCode/Patterns/Bit Manipulation/29. Divide Two Integers/Solution.cpp
+++ LeetCode/Patterns/Bit Manipulation/29. Divide Two Integers/Solution.cpp
@@ -20,6 +20,27 @@ public:
     //     }
     //    return negative ? -i : i;
 
-    
+        //Optimal solution: TC: O(log(dividend)) and SC: O(1)
+        long long sum = 0;
+        long long i = 0;
+        bool isNegative = (dividend < 0) ^ (divisor < 0);
+        long long dummyDividend = dividend;
+        long long dummyDivisor = divisor;
+        long long quotient = 0;
+        if(dummyDividend < 0) dummyDividend = -dummyDividend;
+        if(dummyDivisor < 0) dummyDivisor = -dummyDivisor;
+
+        while(dummyDividend >= dummyDivisor){
+            if(sum + (dummyDivisor << i) > dummyDividend){
+                dummyDividend -= sum;
+                sum = 0;
+                i = 0;
+            }else{
+                sum += dummyDivisor << i;
+                quotient += 1 << i;
+                i++;
+            }
+        }
+        return isNegative ? -(quotient) : quotient;
     }
 };
