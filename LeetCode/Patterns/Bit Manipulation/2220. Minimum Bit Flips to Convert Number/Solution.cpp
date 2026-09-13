27e399b7cbc707f2192eff68e2a8283693e09b55
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-09-13 17:29:07 +0530

    Add Easy C++ Solution for 2220. Minimum Bit Flips to Convert Number from LeetCode

---
+++ LeetCode/Patterns/Bit Manipulation/2220. Minimum Bit Flips to Convert Number/Solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    vector<int> convertToBinary(int num){
+        vector<int> ans(32, 0);
+        int i = 0;
+        while(num > 0){
+            ans[i] = num % 2;
+            num = num/2;
+            i++;
+        }
+        // reverse(ans.begin(), ans.end());
+        return ans;
+    }
+
+    int minBitFlips(int start, int goal) {
+        vector<int> s(32, 0);
+        vector<int> g(32, 0);
+        int count = 0;
+        s = convertToBinary(start);
+        g = convertToBinary(goal);
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] != g[i]){
+                count++;
+                s[i] = g[i];
+            }
+        }
+        return count;
+    }
+};
