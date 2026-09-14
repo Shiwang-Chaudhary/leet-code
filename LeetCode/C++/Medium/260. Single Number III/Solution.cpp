59b5600790ea32e167b51a3bcf695a2663309f07
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-09-14 15:07:06 +0530

    Add Medium C++ Solution for 260. Single Number III from LeetCode

--- LeetCode/C++/Medium/260. Single Number III/Solution.cpp
+++ LeetCode/C++/Medium/260. Single Number III/Solution.cpp
@@ -2,34 +2,16 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         //Brute force: TC: O(n) and SCL O(n)
-        // unordered_map<int,int> mp;
-        // vector<int> ans;
-        // for(int i = 0; i < nums.size(); i++){
-        //     mp[nums[i]]++;
-        // }
-        // for(auto it: mp){
-        //     if(it.second == 1){
-        //         ans.push_back(it.first);
-        //     }
-        // }
-        // return ans;
-
-        //Optimal solution: TC:O(2n) and SC: O(1)
-        int XOR = 0;
-        int el1 = 0;
-        int el2 = 0;
+        unordered_map<int,int> mp;
+        vector<int> ans;
         for(int i = 0; i < nums.size(); i++){
-            XOR ^= nums[i];
+            mp[nums[i]]++;
         }
-        int rightMostBit = (XOR & (XOR-1)) ^ XOR;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] & rightMostBit){
-                //if rightmost bit is of nums[i] is set then it will go to element 1 otherwise element 2
-                el1 ^= nums[i];
-            }else{
-                el2 ^= nums[i];
+        for(auto it: mp){
+            if(it.second == 1){
+                ans.push_back(it.first);
             }
         }
-        return {el1, el2};
+        return ans;
     }
 };
