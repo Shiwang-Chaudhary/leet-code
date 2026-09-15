1c0abf4852539cc47256c9ef4becfd3794aa5d3f
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-09-15 13:06:49 +0530

    Add Medium C++ Solution for 204. Count Primes from LeetCode

---
+++ LeetCode/Patterns/Arrays/204. Count Primes/Solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    //TC of this function: O(√n)
+    bool isPrime(int n){
+        if(n < 2) return false;
+        for(int i = 2; i*i <= n; i++){
+            if(n % i == 0) return false;
+        }
+        return true;
+    }
+
+    int countPrimes(int n) {
+        //Brute force:
+        int count = 0;
+        for(int i = 1; i <= n; i++){
+            if(isPrime(i)) count++;
+        }
+        return count++;
+    }
+};
