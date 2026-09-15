d15d20fb5b5311f40caf3f174c046c1b7704232a
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-09-15 17:15:25 +0530

    Add Medium C++ Solution for 204. Count Primes from LeetCode

--- LeetCode/Patterns/Arrays/204. Count Primes/Solution.cpp
+++ LeetCode/Patterns/Arrays/204. Count Primes/Solution.cpp
@@ -20,22 +20,22 @@ public:
         //Optimal solution using Sieve of Eratosthenes: TC: O(nlog(logn)) and SC: O(k) where k is number of prime numbers
         //mark all numbers as prime initially
         if(n == 0 || n == 1) return 0;
-        vector<int> isPrime(n, 1);
-        isPrime[0] = 0; // 0 is not prime
-        isPrime[1] = 0; // 1 is not prime as well
+        vector<bool> isPrime(n, true);
+        isPrime[0] = false; // 0 is not prime
+        isPrime[1] = false; // 1 is not prime as well
         //we dont actually need to go till n, only till its square root, reason is in excalidraw file
         for(int i = 2; i*i < n; i++){
             if(isPrime[i]){
                 for(int j = i*i; j < n; j = j + i){
                     //j will be composite numbers because they are multiples of i, so mark them composite
-                    isPrime[j] = 0;
+                    isPrime[j] = false;
                 }
             }
         }
         //now all the numbers in isPrime that are marked as 1 are prime.
         int count = 0;
         for(int i = 2; i < isPrime.size(); i++){
-            if(isPrime[i] == 1){
+            if(isPrime[i] == true){
                 count++;
             }
         }
