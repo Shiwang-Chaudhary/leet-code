798160a75abbb35c903bcefa446fdff5492703e3
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-09-18 14:49:46 +0530

    Add Easy C++ Solution for 232. Implement Queue using Stacks from LeetCode

--- LeetCode/Patterns/Stack & Queue/232. Implement Queue using Stacks/Solution.cpp
+++ LeetCode/Patterns/Stack & Queue/232. Implement Queue using Stacks/Solution.cpp
@@ -1,6 +1,5 @@
 class MyQueue {
     stack<int> s1;
-    //basically s2 represent the queue elements
     stack<int> s2;
 public:
     MyQueue() {
@@ -9,61 +8,29 @@ public:
     
     void push(int x) {
         //Approach 1: Costly push....
-        //TC: O(2n) where n is the current size of stack
-        //SC: 2 dynamic stacks
-        // while(!s1.empty()){
-        //     s2.push(s1.top());
-        //     s1.pop();
-        // }
-        // s1.push(x);
-        // while(!s2.empty()){
-        //     s1.push(s2.top());
-        //     s2.pop();
-        // }
-
-        //Approach 2: Costly pop/top approach... (recommended)
+        while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
+        }
         s1.push(x);
+        while(!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
+        }
     }
     
     int pop() {
-        // int element = s1.top();
-        // s1.pop();
-        // return element;
-
-        if(!s2.empty()){
-            int element = s2.top();
-            s2.pop();
-            return element;
-        }else{
-            //put all elements of s1 to s2 and remove top element
-            while(!s1.empty()){
-                s2.push(s1.top());
-                s1.pop();
-            }
-            int element = s2.top();
-            s2.pop();
-            return element;
-        }
+        int element = s1.top();
+        s1.pop();
+        return element;
     }
     
     int peek() {
-        // return s1.top();
-
-        //s2 basically represent the queue elements, if its not empty then pop from s2
-        if(!s2.empty()){
-            return s2.top();
-        }else{
-            //if s2 is empty then put s1 element into s2 and then pop from it
-            while(!s1.empty()){
-                s2.push(s1.top());
-                s1.pop();
-            }
-            return s2.top();
-        }
+        return s1.top();
     }
     
     bool empty() {
-        return s1.empty() && s2.empty();
+        return s1.empty();
     }
 };
 
