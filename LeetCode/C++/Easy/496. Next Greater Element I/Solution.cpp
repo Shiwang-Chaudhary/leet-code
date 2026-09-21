391f7b139343f486829bfb62d6c4ce756178e366
Shiwang Chaudhary <166986194+Shiwang-Chaudhary@users.noreply.github.com>
2026-09-21 16:00:43 +0530

    Add Easy C++ Solution for 496. Next Greater Element I from LeetCode

--- LeetCode/C++/Easy/496. Next Greater Element I/Solution.cpp
+++ LeetCode/C++/Easy/496. Next Greater Element I/Solution.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        //
         int n1 = nums1.size();
         int n2 = nums2.size();
         vector<int> ans(n1, -1);
